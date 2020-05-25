f226a5a30e7f0d3fb454d9e8a2eb6287ea2ae45b
Miguel R. Ávila <miguelravila@outlook.com>
2020-05-24 23:30:48 -0500

    Clock JS

---
+++ app.js
@@ -0,0 +1,13 @@
+const hr = document.getElementById('hr');
+const min = document.getElementById('min');
+const sec = document.getElementById('sec');
+const deg = 6;
+setInterval(() => {
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
+    hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
+    min.style.transform = `rotateZ(${mm}deg)`;
+    sec.style.transform = `rotateZ(${ss}deg)`;
+});
