151295d724d56d05f1cf70c1ec565f38758dfdaf
Miguel R. Ávila <miguelravila@outlook.com>
2020-05-24 23:41:13 -0500

    Bug fixing

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ setInterval(() => {
     let hh = day.getHours() * 30;
     let mm = day.getMinutes() * deg;
     let ss = day.getSeconds() * deg;
-    hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
+    hr.style.transform = `rotateZ(${hh + (mm / 12)}deg)`;
     min.style.transform = `rotateZ(${mm}deg)`;
     sec.style.transform = `rotateZ(${ss}deg)`;
 });
